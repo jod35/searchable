f7940d13462950a57bb3736a3a16b60baede0544
jod35 <jodestrevin@gmail.com>
2021-05-26 16:23:59 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,111 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React , {useState} from 'react'
+import ReactDOM from 'react-dom'
+import './main.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+const notes = [
+  {
+    "title": "I am a developer",
+    "description": "I develop software"
+  },
+  {
+    "title":"Benefits of Using Django",
+    "description": "I develop software"
+  },
+  {
+    "title": "Cool skills",
+    "description": "I develop software"
+  },
+  {
+    "title": "Prop Types",
+    "description": "I develop software"
+  },
+  {
+    "title": "How to be a Ninja",
+    "description": "I develop software"
+  },
+  {
+    "title": "Elephant ways",
+    "description": "I develop software"
+  },
+  {
+    "title": "Jamaican Uganda",
+    "description": "I develop software"
+  },
+  {
+    "title": "Items in my bag",
+    "description": "I develop software"
+  },
+
+];
+
+const Search=(props)=>{
+
+  return(
+      <div className="search">
+        
+        <label htmlFor="search" />
+        <input type="text" onChange={props.onSearch} value={props.search}/>
+        <hr />
+      </div>
+  ) 
+}
+
+const List=(props)=>{
+  return(
+      <div className="notes">
+        {
+          props.list.map((item,index)=>{
+            return(
+            <div className="note" key={index}>
+               <h1>{item.title}</h1>
+               <p>{item.description}</p>
+            </div>)
+          })
+
+        }
+      </div>
+  )
+}
+
+
+function App(){
+
+
+  const title = "App Home"
+  const [searchTerm, setSearchTerm] = useState(
+    localStorage.getItem('search') || 'React'
+  )
+
+  const searchedStories=notes.filter(
+    (note) =>{
+      return note.title
+      .toLowerCase()
+      .includes(searchTerm)
+    }
+  )
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+
+    localStorage.setItem('search', e.target.value)
+    console.log(searchTerm)
+  }
+
+  return(
+    <div className="app">
+
+      <h1>{title}</h1>
+      <Search onSearch={handleSearch} search={searchTerm}/>
+      <p>Searching for: {searchTerm}</p>
+      
+      <List list={searchedStories}/>
+      
+    </div>
+  )
+}
+
+
+ReactDOM.render(<App/>,document.getElementById('root'));
